8880d83eec78277a9db2c5f352175501b5b0b8dd
Gustavo Monteiro <ggpm20@hotmail.com.br>
2024-01-11 16:24:18 -0300

    updated user model

--- src/models/User.js
+++ src/models/User.js
@@ -46,6 +46,8 @@ const UserSchema = new mongoose.Schema({
 });
 
 async function encryptUserPassword(next) {
+  if (!this.isModified('password')) return next();
+
   this.password = await bcrypt.hash(this.password, 10);
   next();
 }
